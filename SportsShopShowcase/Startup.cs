ef96d8a20a45b704e24237a8264acb6e3e44d5ff
Andrew Oakley <oakleyandrewj@gmail.com>
2021-04-04 21:24:15 +0100

    Added services call for controllers with views and added pipeline calls for https, static files. Added a default endpoint config for the url pattern

--- SportsShopShowcase/Startup.cs
+++ SportsShopShowcase/Startup.cs
@@ -12,28 +12,28 @@ namespace SportsShopShowcase
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        //called by the runtime
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //used to configure the request pipeline
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name:"default",
+                    pattern:"{Controller=Home}/{action=Index}/{id?}");
             });
         }
     }
